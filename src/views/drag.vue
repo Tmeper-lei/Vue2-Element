1595d73f587aa393ef64a39dd131611f2d783591
Temper-lei <728705057@qq.com>
2023-05-20 22:46:48 +0800

    Vue2

---
+++ src/views/drag.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+
+    <div class="some-panel">
+      <div v-drag drag-min-top="50" style="height: 20px; background-color: blue"></div>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+.some-panel {
+  width: 200px;
+  height: 140px;
+  position: absolute;
+  background-color: coral;
+  margin-top: 100px;
+}
+.some-panel[dragged] {
+  height: 200px;
+}
+</style>
